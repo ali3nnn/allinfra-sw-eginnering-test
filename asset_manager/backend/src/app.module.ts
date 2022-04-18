005169e9fbe57fa06ad4348af99a13dd0418ce70
Barbu <abarbu@deloittece.com>
2022-04-18 10:40:07 +0300

    small update

--- asset_manager/backend/src/app.module.ts
+++ asset_manager/backend/src/app.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AssetsService } from './assets/assets.service';
-import { AssetsController } from './assets/assets.controller';
 import { AssetsModule } from './assets/assets.module';
-import { Assets } from './assets/entities/asset.entity';
 
 @Module({
   imports: [AssetsModule],
